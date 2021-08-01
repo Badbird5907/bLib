cba6ba84fa547b71953193e261eba2925c1c8023
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-08-01 16:02:23 -0400

    Update bLib.java

--- bLib-Bukkit/src/main/java/net/badbird5907/blib/bLib.java
+++ bLib-Bukkit/src/main/java/net/badbird5907/blib/bLib.java
@@ -10,17 +10,17 @@ import org.bukkit.plugin.Plugin;
 @Getter
 @Setter
 public class bLib {
-    private static bLib instance;
-    @Getter
-    @Setter
-    private static Plugin plugin;
-    @Getter
-    private static CommandFramework commandFramework;
-    public bLib(Plugin plugin,String prefix){
-        instance = this;
-        setPlugin(plugin);
-        new Logger(plugin.getLogger(),prefix,"[DEBUG]");
-        new Tasks(plugin);
-        commandFramework = new CommandFramework(plugin);
-    }
+	private static bLib instance;
+	@Getter
+	@Setter
+	private static Plugin plugin;
+	@Getter
+	private static CommandFramework commandFramework;
+	public bLib(Plugin plugin,String prefix){
+		instance = this;
+		setPlugin(plugin);
+		new Logger(plugin.getLogger(),prefix,"[DEBUG]");
+		new Tasks(plugin);
+		commandFramework = new CommandFramework(plugin);
+	}
 }

235ed638e8213516d4b1952f83c1d345eb293e74
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-08-01 16:33:01 -0400

    Update BukkitCommand.java

--- bLib-Bukkit/src/main/java/net/badbird5907/blib/command/BukkitCommand.java
+++ bLib-Bukkit/src/main/java/net/badbird5907/blib/command/BukkitCommand.java
@@ -18,7 +18,6 @@ import java.util.List;
  * 
  */
 public class BukkitCommand extends org.bukkit.command.Command {
-
 	private final Plugin owningPlugin;
 	private CommandExecutor executor;
 	protected BukkitCompleter completer;
@@ -39,61 +38,34 @@ public class BukkitCommand extends org.bukkit.command.Command {
 	@Override
 	public boolean execute(CommandSender sender, String commandLabel, String[] args) {
 		boolean success = false;
-
-		if (!owningPlugin.isEnabled()) {
-			return false;
-		}
-
-		if (!testPermission(sender)) {
-			return true;
-		}
-
+		if (!owningPlugin.isEnabled()) return false;
+		if (!testPermission(sender)) return true;
 		try {
 			success = executor.onCommand(sender, this, commandLabel, args);
 		} catch (Throwable ex) {
-			throw new CommandException("Unhandled exception executing command '" + commandLabel + "' in plugin "
-					+ owningPlugin.getDescription().getFullName(), ex);
+			throw new CommandException("Unhandled exception executing command '" + commandLabel + "' in plugin " + owningPlugin.getDescription().getFullName(), ex);
 		}
-
-		if (!success && usageMessage.length() > 0) {
-			for (String line : usageMessage.replace("<command>", commandLabel).split("\n")) {
-				sender.sendMessage(line);
-			}
-		}
-
+		if (!success && usageMessage.length() > 0) for (String line : usageMessage.replace("<command>", commandLabel).split("\n")) sender.sendMessage(line);
 		return success;
 	}
 
 	@Override
-	public List<String> tabComplete(CommandSender sender, String alias, String[] args)
-			throws CommandException, IllegalArgumentException {
+	public List<String> tabComplete(CommandSender sender, String alias, String[] args) throws CommandException, IllegalArgumentException {
 		Validate.notNull(sender, "Sender cannot be null");
 		Validate.notNull(args, "Arguments cannot be null");
 		Validate.notNull(alias, "Alias cannot be null");
-
 		List<String> completions = null;
 		try {
-			if (completer != null) {
-				completions = completer.onTabComplete(sender, this, alias, args);
-			}
-			if (completions == null && executor instanceof TabCompleter) {
-				completions = ((TabCompleter) executor).onTabComplete(sender, this, alias, args);
-			}
+			if (completer != null) completions = completer.onTabComplete(sender, this, alias, args);
+			if (completions == null && executor instanceof TabCompleter) completions = ((TabCompleter) executor).onTabComplete(sender, this, alias, args);
 		} catch (Throwable ex) {
 			StringBuilder message = new StringBuilder();
 			message.append("Unhandled exception during tab completion for command '/").append(alias).append(' ');
-			for (String arg : args) {
-				message.append(arg).append(' ');
-			}
-			message.deleteCharAt(message.length() - 1).append("' in plugin ")
-					.append(owningPlugin.getDescription().getFullName());
+			for (String arg : args) message.append(arg).append(' ');
+			message.deleteCharAt(message.length() - 1).append("' in plugin ").append(owningPlugin.getDescription().getFullName());
 			throw new CommandException(message.toString(), ex);
 		}
-
-		if (completions == null) {
-			return super.tabComplete(sender, alias, args);
-		}
+		if (completions == null) return super.tabComplete(sender, alias, args);
 		return completions;
 	}
-
 }

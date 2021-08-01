c494879c7e7dd3f4892f868bac2a944fb96fb885
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-08-01 16:25:52 -0400

    Update Tasks.java

--- bLib-Bukkit/src/main/java/net/badbird5907/blib/util/Tasks.java
+++ bLib-Bukkit/src/main/java/net/badbird5907/blib/util/Tasks.java
@@ -4,33 +4,37 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 
 public class Tasks {
-    private static Plugin plugin;
-    public Tasks(Plugin plugin1){
-        plugin = plugin1;
-    }
+	private static Plugin plugin;
 
-    public static void run( Runnable callable) {
-        Bukkit.getScheduler().runTask(plugin, callable);
-    }
-    public static void runSync(Runnable callable){run(callable);}
+	public Tasks(Plugin plugin1) {
+		plugin = plugin1;
+	}
 
-    public static void runAsync( Runnable callable) {
-        Bukkit.getScheduler().runTaskAsynchronously(plugin, callable);
-    }
+	public static void run( Runnable callable) {
+		Bukkit.getScheduler().runTask(plugin, callable);
+	}
 
-    public static void runLater( Runnable callable, long delay) {
-        Bukkit.getScheduler().runTaskLater(plugin, callable, delay);
-    }
+	public static void runSync(Runnable callable) {
+		run(callable);
+	}
 
-    public static void runAsyncLater( Runnable callable, long delay) {
-        Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, callable, delay);
-    }
+	public static void runAsync( Runnable callable) {
+		Bukkit.getScheduler().runTaskAsynchronously(plugin, callable);
+	}
 
-    public static void runTimer( Runnable callable, long delay, long interval) {
-        Bukkit.getScheduler().runTaskTimer(plugin, callable, delay, interval);
-    }
+	public static void runLater( Runnable callable, long delay) {
+		Bukkit.getScheduler().runTaskLater(plugin, callable, delay);
+	}
 
-    public static void runAsyncTimer( Runnable callable, long delay, long interval) {
-        Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, callable, delay, interval);
-    }
+	public static void runAsyncLater( Runnable callable, long delay) {
+		Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, callable, delay);
+	}
+
+	public static void runTimer( Runnable callable, long delay, long interval) {
+		Bukkit.getScheduler().runTaskTimer(plugin, callable, delay, interval);
+	}
+
+	public static void runAsyncTimer( Runnable callable, long delay, long interval) {
+		Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, callable, delay, interval);
+	}
 }

3c31719cc6091cf5fbf6ac23431ec5e1ae347d84
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-08-01 16:13:51 -0400

    Update CC.java

--- bLib-Bukkit/src/main/java/net/badbird5907/blib/util/CC.java
+++ bLib-Bukkit/src/main/java/net/badbird5907/blib/util/CC.java
@@ -1,64 +1,29 @@
 package net.badbird5907.blib.util;
 
-import net.md_5.bungee.api.ChatColor;
-
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static net.md_5.bungee.api.ChatColor.*;
+import static java.util.stream.Collectors.*;
 
 public final class CC {
-    public static final String WHITE = ChatColor.WHITE.toString();
-    public static final String GREEN = ChatColor.GREEN.toString();
-    public static final String D_GREEN = ChatColor.DARK_GREEN.toString();
-    public static final String D_BLUE = ChatColor.DARK_BLUE.toString();
-    public static final String RED = ChatColor.RED.toString();
-    public static final String D_RED = ChatColor.DARK_RED.toString();
-    public static final String GRAY = ChatColor.GRAY.toString();
-    public static final String D_GRAY = ChatColor.DARK_GRAY.toString();
-    public static final String YELLOW = ChatColor.YELLOW.toString();
-    public static final String GOLD = ChatColor.GOLD.toString();
-    public static final String AQUA = ChatColor.AQUA.toString();
-    public static final String D_AQUA = ChatColor.DARK_AQUA.toString();
-    public static final String BLUE = ChatColor.BLUE.toString();
-    public static final String PINK = ChatColor.LIGHT_PURPLE.toString();
-    public static final String PURPLE = ChatColor.DARK_PURPLE.toString();
-    public static final String BLACK = ChatColor.BLACK.toString();
-    public static final String B = ChatColor.BOLD.toString();
-    public static final String I = ChatColor.ITALIC.toString();
-    public static final String U = ChatColor.UNDERLINE.toString();
-    public static final String S = ChatColor.STRIKETHROUGH.toString();
-    public static final String R = ChatColor.RESET.toString();
-    public static final String BOLD = ChatColor.BOLD.toString();
-    public static final String ITALIC = ChatColor.ITALIC.toString();
-    public static final String UNDERLINE = ChatColor.UNDERLINE.toString();
-    public static final String STRIKETHROUGH = ChatColor.STRIKETHROUGH.toString();
-    public static final String RESET = ChatColor.RESET.toString();
-    public static final String PRIMARY = ChatColor.AQUA.toString();
-    public static final String SECONDARY = ChatColor.LIGHT_PURPLE.toString();
-    public static final String ACCENT = ChatColor.DARK_AQUA.toString();
-    public static final String SPLITTER = "┃";
-    public static final String SCOREBOARD_SEPARATOR = GRAY + S + "--------------------";
-    public static final String SCOREBOARD_IP_SEPARATOR = GRAY + S + "---";
-    public static final String SEPARATOR = GRAY + S + "-------------------------------------";
-    public static final String BULLET = "•";
-    public static final String NEWLINE = "\n";
-    public static final String NL = "\n";
-    public static final String ARROW_RIGHT = "»";
-    public static final String ARROW_LEFT = "«";
-    public static final String X = "✘";
-    public static final String CHECK = "\u2714";
-    public static final String SELECTOR_ARROW = "\u25b8";
+	public static final String WHITE = WHITE.toString(), GREEN = GREEN.toString(), D_GREEN = DARK_GREEN.toString(), D_BLUE = DARK_BLUE.toString(), RED = RED.toString(), D_RED = DARK_RED.toString(), GRAY = GRAY.toString(), D_GRAY = DARK_GRAY.toString(), YELLOW = YELLOW.toString(), GOLD = GOLD.toString(), AQUA = AQUA.toString(), D_AQUA = DARK_AQUA.toString(), BLUE = BLUE.toString(), PINK = LIGHT_PURPLE.toString(), PURPLE = DARK_PURPLE.toString(), BLACK = BLACK.toString();
+	public static final String B = BOLD.toString(), BOLD = BOLD.toString(), I = ITALIC.toString(), ITALIC = ITALIC.toString(), U = UNDERLINE.toString(), UNDERLINE = UNDERLINE.toString(), S = STRIKETHROUGH.toString(), STRIKETHROUGH = STRIKETHROUGH.toString(), R = RESET.toString(), RESET = RESET.toString();
+	public static final String PRIMARY = AQUA.toString(), SECONDARY = LIGHT_PURPLE.toString(), ACCENT = DARK_AQUA.toString();
+	public static final String SPLITTER = "┃", SCOREBOARD_SEPARATOR = GRAY + S + "--------------------", SCOREBOARD_IP_SEPARATOR = GRAY + S + "---", SEPARATOR = GRAY + S + "-------------------------------------", BULLET = "•", NEWLINE = "\n", NL = "\n", ARROW_RIGHT = "»", ARROW_LEFT = "«", X = "✘", CHECK = "\u2714", SELECTOR_ARROW = "\u25b8";
 
-    public static String translate(String in){
-        return ChatColor.translateAlternateColorCodes('&',in);
-    }
-    public static List<String> translate(List<String> input) {
-        return input.stream().map(CC::translate).collect(Collectors.toList());
-    }
-    public static String strip(String in){
-        return ChatColor.stripColor(in);
-    }
+	public static String translate(String in) {
+		return translateAlternateColorCodes('&', in);
+	}
 
-    private CC() {
-        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
-    }
+	public static List<String> translate(List<String> input) {
+		return input.stream().map(CC::translate).collect(toList());
+	}
+
+	public static String strip(String in) {
+		return stripColor(in);
+	}
+
+	private CC() {
+		throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+	}
 }

31d2c4daee66bcb2ec07de11a2d5b1045594426a
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-08-07 14:09:45 -0400

    Update FileUtils.java

--- bLib-Common/src/main/java/net/badbird5907/blib/utils/FileUtils.java
+++ bLib-Common/src/main/java/net/badbird5907/blib/utils/FileUtils.java
@@ -8,13 +8,13 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileUtils {
-    public static String readFileToString(File file){
-        StringBuilder contentBuilder = new StringBuilder();
-        try (Stream<String> stream = Files.lines(Paths.get(file.getPath()), StandardCharsets.UTF_8)) {
-            stream.forEach(s -> contentBuilder.append(s).append("\n")); //not the best solution but it works
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return contentBuilder.toString();
-    }
+	public static String readFileToString(File file) {
+		StringBuilder contentBuilder = new StringBuilder();
+		try (Stream<String> stream = Files.lines(Paths.get(file.getPath()), StandardCharsets.UTF_8)) {
+			stream.forEach(s -> contentBuilder.append(s).append("\n")); // not the best solution but it works
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return contentBuilder.toString();
+	}
 }

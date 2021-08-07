5f9a0757f1036eb05a76a8c490eb6001201b58ba
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-08-07 13:52:28 -0400

    Update PairMap.java

--- bLib-Common/src/main/java/net/badbird5907/blib/objects/maps/pair/PairMap.java
+++ bLib-Common/src/main/java/net/badbird5907/blib/objects/maps/pair/PairMap.java
@@ -4,37 +4,47 @@ import net.badbird5907.blib.objects.TriConsumer;
 import net.badbird5907.blib.objects.tuple.Pair;
 import java.util.*;
 
-public interface PairMap<K,V,M>{
-    interface Entry<K,V,M>{
-        K getKey();
-        V getValue1();
-        M getValue2();
-    }
-    Set<Entry<K,V,M>> entrySet();
-    Set<Pair<V,M>> values();
-    Set<K> keySet();
-    void clear();
-    void putAll(PairMap<? extends K,? extends V,? extends M > e);
-    V remove(Object k);
-    V put(K key,V value1,M value2);
-    Pair<V,M> get(K key);
-    boolean containsValue(Object val);
-    boolean containsKey(Object val);
-    int size();
-    default void forEach(TriConsumer<? super K,? super V,? super M> action){
-        Objects.requireNonNull(action);
-        for (Entry<K,V,M> entry : entrySet()) {
-            K k;
-            V v;
-            M m;
-            try{
-                k = entry.getKey();
-                v = entry.getValue1();
-                m = entry.getValue2();
-            } catch (IllegalStateException e) {
-                throw new ConcurrentModificationException(e);
-            }
-            action.accept(k,v,m);
-        }
-    }
+public interface PairMap<K, V, M> {
+	interface Entry<K, V, M> {
+		K getKey();
+		V getValue1();
+		M getValue2();
+	}
+
+	Set<Entry<K,V,M>> entrySet();
+	Set<Pair<V,M>> values();
+	Set<K> keySet();
+
+	void clear();
+
+	void putAll(PairMap<? extends K,? extends V,? extends M > e);
+
+	V remove(Object k);
+
+	V put(K key, V value1, M value2);
+
+	Pair<V, M> get(K key);
+
+	boolean containsValue(Object val);
+
+	boolean containsKey(Object val);
+
+	int size();
+
+	default void forEach(TriConsumer<? super K, ? super V, ? super M> action){
+		Objects.requireNonNull(action);
+		for (Entry<K, V, M> entry : entrySet()) {
+			K k;
+			V v;
+			M m;
+			try {
+				k = entry.getKey();
+				v = entry.getValue1();
+				m = entry.getValue2();
+			} catch (IllegalStateException e) {
+				throw new ConcurrentModificationException(e);
+			}
+			action.accept(k, v, m);
+		}
+	}
 }

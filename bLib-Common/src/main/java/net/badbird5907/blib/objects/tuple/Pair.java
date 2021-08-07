c624a4d63f338f8cc5d0399e510190826fed7665
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-08-07 14:02:20 -0400

    Update Pair.java

--- bLib-Common/src/main/java/net/badbird5907/blib/objects/tuple/Pair.java
+++ bLib-Common/src/main/java/net/badbird5907/blib/objects/tuple/Pair.java
@@ -5,27 +5,28 @@ package net.badbird5907.blib.objects.tuple;
  * @param <A> value 1
  * @param <B> value 2
  */
-public class Pair<A,B>{
-    private A value0;
-    private B value1;
-    public Pair(final A v0,B v1){
-        this.value0 = v0;
-        this.value1 = v1;
-    }
+public class Pair<A, B> {
+	private A value0;
+	private B value1;
 
-    /**
-     * gets the first value
-     * @return
-     */
-    public A getValue0() {
-        return value0;
-    }
+	public Pair(A v0, B v1){
+		this.value0 = v0;
+		this.value1 = v1;
+	}
 
-    /**
-     * gets the second value
-     * @return
-     */
-    public B getValue1() {
-        return value1;
-    }
+	/**
+	 * gets the first value
+	 * @return
+	 */
+	public A getValue0() {
+		return value0;
+	}
+
+	/**
+	 * gets the second value
+	 * @return
+	 */
+	public B getValue1() {
+		return value1;
+	}
 }

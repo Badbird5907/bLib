585c70f5e22f4b8cb9eea6dd4d81343cf592edfd
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-08-07 14:07:26 -0400

    Update TriConsumer.java

--- bLib-Common/src/main/java/net/badbird5907/blib/objects/TriConsumer.java
+++ bLib-Common/src/main/java/net/badbird5907/blib/objects/TriConsumer.java
@@ -8,13 +8,14 @@ import java.util.Objects;
  * @param <B>
  * @param <C>
  */
-public interface TriConsumer<A,B,C>{
-    void accept(A a,B b,C c);
-    default TriConsumer<A,B,C> andThen(TriConsumer<? super A, ? super B, ? super C> after){
-        Objects.requireNonNull(after);
-        return (a,b,c) ->{
-            accept(a,b,c);
-            after.accept(a,b,c);
-        };
-    }
+public interface TriConsumer<A, B, C> {
+	void accept(A a, B b, C c);
+
+	default TriConsumer<A, B, C> andThen(TriConsumer<? super A, ? super B, ? super C> after) {
+		Objects.requireNonNull(after);
+		return (a, b, c) -> {
+			accept(a, b, c);
+			after.accept(a, b, c);
+		};
+	}
 }

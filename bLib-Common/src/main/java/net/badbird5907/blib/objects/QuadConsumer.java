8d36e1d81252ec2799b43f2ec75f95246699c106
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-08-07 14:06:10 -0400

    Update QuadConsumer.java

--- bLib-Common/src/main/java/net/badbird5907/blib/objects/QuadConsumer.java
+++ bLib-Common/src/main/java/net/badbird5907/blib/objects/QuadConsumer.java
@@ -9,13 +9,14 @@ import java.util.Objects;
  * @param <C> input 3
  * @param <D> input 4
  */
-public interface QuadConsumer<A,B,C,D>{
-    void accept(A a,B b,C c,D d);
-    default QuadConsumer<A,B,C,D> andThen(QuadConsumer<? super A, ? super B, ? super C,? super D> after){
-        Objects.requireNonNull(after);
-        return (a,b,c,d) ->{
-            accept(a,b,c,d);
-            after.accept(a,b,c,d);
-        };
-    }
+public interface QuadConsumer<A, B, C, D> {
+	void accept(A a, B b, C c, D d);
+
+	default QuadConsumer<A, B, C, D> andThen(QuadConsumer<? super A, ? super B, ? super C, ? super D> after) {
+		Objects.requireNonNull(after);
+		return (a, b, c, d) -> {
+			accept(a, b, c, d);
+			after.accept(a, b, c, d);
+		};
+	}
 }

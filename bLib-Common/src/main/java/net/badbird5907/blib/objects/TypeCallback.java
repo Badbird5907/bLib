37d874f0013ba714a1db0116793abac7b04ccadd
Badbird-5907 <evan.lele.yu2@gmail.com>
2021-11-08 21:31:19 -0500

    Revert "fix TypeCallback, added QuestionConversation"
    This reverts commit 07a56727b8fd4205e704899013d107d9a67dac81.

--- bLib-Common/src/main/java/net/badbird5907/blib/objects/TypeCallback.java
+++ bLib-Common/src/main/java/net/badbird5907/blib/objects/TypeCallback.java
@@ -1,14 +1,9 @@
 package net.badbird5907.blib.objects;
 
-import java.io.Serializable;
-
-public interface TypeCallback<T,B> extends Serializable {
-
-    /**
-     * A callback for running a task on a set of data.
-     *
-     * @param data the data needed to run the task.
-     */
-    T callback(B data);
-
+/**
+ * {@link Callback} but with a return type
+ * @param <A>
+ */
+public interface TypeCallback<A> {
+    A call();
 }
